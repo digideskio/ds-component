6879d20b6d511de4bfd3c4fe0579131bab969369
Matt Hamrick <matthewjhamrick@gmail.com>
2018-10-25 17:17:30 -0700

    Updated react example.

--- examples/react-ts/src/App.tsx
+++ examples/react-ts/src/App.tsx
@@ -1,32 +1,36 @@
 import * as dscc from '@google/dscc';
 import * as React from 'react';
 
-const Message = (message: dscc.Message) => {
-  const indexed = dscc.fieldsById(message);
+const Message = (message: dscc.TableFormat) => {
+  // const indexed = dscc.fieldsById(message);
   return (
     <table>
       <thead>
         <tr>
-          {message.dataResponse.tables[0].fields.map((field, idx) => (
-            <td key={`heading-${idx}`}>{indexed[field].name}</td>
-          ))}
-        </tr>
+      {
+        message.tables[dscc.TableType.DEFAULT][0].map((conceptName, idx) => (
+            <td key={`heading-${idx}`}>{conceptName}</td>
+        ))
+      }
+    </tr>
       </thead>
       <tbody>
-        {message.dataResponse.tables[0].rows.map((row, idx) => (
-          <tr key={`tableRow-${idx}`}>
+      {
+        message.tables[dscc.TableType.DEFAULT].slice(1).map((row, idx) => (
+            <tr key={`tableRow-${idx}`}>
             {row.map((rowEntry, innerIdx) => (
-              <td key={`tableRow-${idx}-value-${innerIdx}`}>{rowEntry}</td>
+                <td key={`tableRow-${idx}-value-${innerIdx}`}>{rowEntry}</td>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        ))
+      }
+    </tbody>
+      </table>
   );
 };
 
 interface State {
-  message?: dscc.Message;
+  message?: dscc.TableFormat;
 }
 
 class App extends React.Component<{}, State> {
@@ -39,7 +43,7 @@ class App extends React.Component<{}, State> {
     const reactThis = this;
     dscc.subscribeToData((message) => {
       reactThis.setState({message});
-    });
+    }, {transform: dscc.tableTransform});
   }
 
   public render() {
@@ -62,68 +66,66 @@ class App extends React.Component<{}, State> {
 
 export default App;
 
-// setTimeout(() => {
-//   const message: dscc.Message = {
-//     config: {
-//       data: [
-//         {
-//           elements: [
-//             {
-//               id: 'hi',
-//               label: 'hi',
-//               options: {
-//                 max: 1,
-//                 min: 1,
-//               },
-//               type: dscc.ConfigDataElementType.DIMENSION,
-//               value: ['field1'],
-//             },
-//             {
-//               id: 'there',
-//               label: 'there',
-//               options: {
-//                 max: 1,
-//                 min: 1,
-//               },
-//               type: dscc.ConfigDataElementType.METRIC,
-//               value: ['field2'],
-//             },
-//           ],
-//           id: 'hi',
-//           label: 'hi',
-//         },
-//       ],
-//       style: {
-//         elements: [],
-//         id: 'hi',
-//         label: 'hi',
-//       },
-//       themeStyle: undefined,
-//     },
-//     dataResponse: {
-//       tables: [
-//         {
-//           fields: ['field1', 'field2'],
-//           id: dscc.TableType.DEFAULT,
-//           rows: [['matt', 1], ['yulan', 3], ['minhaz', 5]],
-//         },
-//       ],
-//     },
-//     fields: [
-//       {
-//         description: "A person's name.",
-//         id: 'field1',
-//         name: 'Name',
-//         type: dscc.FieldType.TEXT,
-//       },
-//       {
-//         description: "A person's favorite number.",
-//         id: 'field2',
-//         name: 'Favorite Number',
-//         type: dscc.FieldType.NUMBER,
-//       },
-//     ],
-//     type: dscc.MessageType.RENDER,
-//   };
-//   window.parent.postMessage(message, '*');
-// }, 1000);
+setTimeout(() => {
+  const message: dscc.Message = {
+    config: {
+      data: [
+        {
+          elements: [
+            {
+              id: 'hi',
+              label: 'hi',
+              options: {
+                max: 1,
+                min: 1,
+              },
+              type: dscc.ConfigDataElementType.DIMENSION,
+              value: ['field1'],
+            },
+            {
+              id: 'there',
+              label: 'there',
+              options: {
+                max: 1,
+                min: 1,
+              },
+              type: dscc.ConfigDataElementType.METRIC,
+              value: ['field2'],
+            },
+          ],
+          id: 'hi',
+          label: 'hi',
+        },
+      ],
+      style: [],
+      themeStyle: undefined,
+    },
+    dataResponse: {
+      tables: [
+        {
+          fields: ['field1', 'field2'],
+          id: dscc.TableType.DEFAULT,
+          rows: [['matt', 1], ['yulan', 3], ['minhaz', 5]],
+        },
+      ],
+    },
+    fields: [
+      {
+        concept: dscc.ConceptType.DIMENSION,
+        description: "A person's name.",
+        id: 'field1',
+        name: 'Name',
+        type: dscc.FieldType.TEXT,
+      },
+      {
+        concept: dscc.ConceptType.METRIC,
+        description: "A person's favorite number.",
+        id: 'field2',
+        name: 'Favorite Number',
+        type: dscc.FieldType.NUMBER,
+      },
+    ],
+    type: dscc.MessageType.RENDER,
+  };
+  window.parent.postMessage(message, '*');
+}, 1000);
